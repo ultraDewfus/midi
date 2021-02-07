5d551872309cfe81e00e2ee10a1566d303ad71a4
ultraDewfus <ultradewfus@gmail.com>
2021-02-06 19:32:11 -0700

    Initialize and AudioContext.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import MIDIDevice from "./MIDIDevice";
 function main() {
   new MIDIDevice(); 
+  const audioCtx = new AudioContext();
+  console.log('AudioContext state: ' + audioCtx.state);
 }
 
 main();
